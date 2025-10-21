b42951da9b27b5cc5a4cacdf620c7986240e1496
Said Echlahbi <sechlahb@c3r9p8.1337.ma>
2025-10-21 01:35:55 +0100

    GG

---
+++ Module01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 01:28:51 by sechlahb          #+#    #+#             */
+/*   Updated: 2025/10/21 01:35:41 by sechlahb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+class Zombie
+{
+    private:
+        string data;
+    public:
+        Zombie();
+        void announce( void ); // std::cout << "BraiiiiiiinnnzzzZ..." << std::endl;ls
+        
+}
+
+
+#endif /*ZOMBIE_HPP*/
