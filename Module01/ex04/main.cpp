b68fa3215dac6a6ce51c96d0e0c01e0eb44ce17a
Said Echlahbi <sechlahb@c3r2p14.1337.ma>
2025-10-26 18:47:41 +0100

    sed for losers

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,18 +6,69 @@
 /*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 23:39:38 by sechlahb          #+#    #+#             */
-/*   Updated: 2025/10/25 23:04:53 by sechlahb         ###   ########.fr       */
+/*   Updated: 2025/10/26 18:46:51 by sechlahb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
+std::string replaceAll(std::string line, std::string s1, std::string s2)
+{
+    std::string result;
+    std::size_t last_pos = 0;
+    std::size_t pos = line.find(s1);
+    
+    while (pos != std::string::npos)
+    {
+        result.append(line, last_pos, pos - last_pos);
+        result.append(s2);
+        last_pos = pos + s1.length();
+        pos = line.find(s1, last_pos);
+    }
+    result.append(line, last_pos, line.length() - last_pos);
+    return result;
+}
 
 int main (int ac, char **av)
 {
-    string line;
-    
-    if (ac == 3)
+    if (ac == 4)
     {
-            
+        if (av[1][0] == 0 || av[2][0] == 0 ||av[3][0] == 0)
+        {
+            std::cerr << "Error: the argument must have value!" << std::endl;
+            return 1;
+        }
+        std::fstream inputfile; 
+        std::fstream outputfile;
+        std::string line;
+
+        inputfile.open(av[1], std::ios::in);
+        if (!inputfile)
+        {
+            std::cerr << "Error: could not open the inputfile." << std::endl;
+            return 1;
+        }
+
+        outputfile.open("./sed.replace", std::ios::out);
+        if (!outputfile)
+        {
+            std::cerr << "Error: could not open the outputfile." << std::endl;
+            inputfile.close();
+            return 1;
+        }
+        
+        while (std::getline(inputfile, line))
+        {
+            std::cout << line << std::endl;
+            std::cout << replaceAll(line, av[2], av[3]) << std::endl << std::endl;
+        }
+        inputfile.close();
+        outputfile.close();
+        return 0;
     }
+    std::cerr << "Error: invalid arguments!" << std::endl;
+    return 1;   
 }
