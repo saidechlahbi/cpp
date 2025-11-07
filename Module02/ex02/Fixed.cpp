3829151ea913d777fe50f9df3b4db66016ec7bb1
said echlahbi <echlahbisaid@gmail.com>
2025-11-07 18:49:04 +0100

    update

---
+++ Module02/ex02/Fixed.cpp
@@ -0,0 +1,187 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 15:07:57 by sechlahb          #+#    #+#             */
+/*   Updated: 2025/11/07 18:48:04 by sechlahb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int _fractionalBits = 8;
+
+
+int Fixed::getRawBits( void ) const
+{
+    return _fixedPointValue;
+}
+void Fixed::setRawBits( int const raw )
+{
+    _fixedPointValue = raw;
+}
+    
+float   Fixed::toFloat() const
+{
+    return static_cast<float>(_fixedPointValue) / (1 << _fractionalBits);
+}
+int     Fixed::toInt() const
+{
+    return _fixedPointValue >> _fractionalBits;
+}
+
+
+/*the Orthodox Canonical Form*/
+
+Fixed::Fixed(): _fixedPointValue(0){}
+
+Fixed::Fixed(const int n)
+{
+    _fixedPointValue = n << _fractionalBits;
+}
+
+Fixed::Fixed(const float f)
+{
+    _fixedPointValue = static_cast<int> (f) * (1 << _fractionalBits);
+}
+
+Fixed::Fixed(const Fixed& original)
+{
+    _fixedPointValue = original.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed& original)
+{
+    _fixedPointValue = original.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed(){}
+
+/*comparison operators*/
+
+bool Fixed::operator>(const Fixed& original)
+{
+    if (_fixedPointValue > original.getRawBits())
+        return true;
+    return false;
+}
+
+bool Fixed::operator<(const Fixed& original)
+{
+    if (_fixedPointValue < original.getRawBits())
+        return true;
+    return false;
+}
+
+bool Fixed::operator>=(const Fixed& original)
+{
+    if (_fixedPointValue >= original.getRawBits())
+        return true;
+    return false;
+}
+
+bool Fixed::operator<=(const Fixed& original)
+{
+    if (_fixedPointValue <= original.getRawBits())
+        return true;
+    return false;
+}
+
+bool Fixed::operator==(const Fixed& original)
+{
+    if (_fixedPointValue == original.getRawBits())
+        return true;
+    return false;
+}
+
+bool Fixed::operator!=(const Fixed& original)
+{
+    if (_fixedPointValue != original.getRawBits())
+        return true;
+    return false;
+}
+
+/*arithmetic operators*/
+
+Fixed& Fixed::operator+(const Fixed& original)
+{
+    _fixedPointValue += original.getRawBits();
+    return *this;
+}
+Fixed& Fixed::operator-(const Fixed& original)
+{
+    _fixedPointValue -= original.getRawBits();
+    return *this;
+}
+Fixed& Fixed::operator*(const Fixed& original)
+{
+    _fixedPointValue *= original.getRawBits();
+    return *this;
+}
+Fixed& Fixed::operator/(const Fixed& original)
+{
+    _fixedPointValue /= original.getRawBits();
+    return *this;
+}
+
+/*increment and decrement*/
+
+Fixed& Fixed::operator++(int)
+{
+    _fixedPointValue++;
+    return *this;
+}
+Fixed& Fixed::operator--(int)
+{
+    _fixedPointValue--;
+    return *this;
+}
+Fixed& Fixed::operator++()
+{
+    Fixed ptr = *this;
+    _fixedPointValue++;
+    return ptr;
+}
+Fixed& Fixed::operator--()
+{
+    Fixed ptr = *this;
+    _fixedPointValue--;
+    return ptr;
+}
+
+
+Fixed& Fixed::min(Fixed& a, Fixed& b)
+{
+    if (a.getRawBits() < b.getRawBits())
+        return a;
+    return b;
+}
+Fixed& Fixed::min(const Fixed& a, const Fixed& b)
+{
+    if (a.getRawBits() < b.getRawBits())
+        return a;
+    return b;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b)
+{
+    if (a.getRawBits() > b.getRawBits())
+        return a;
+    return b;
+}
+Fixed& Fixed::max(const Fixed& a, const Fixed& b)
+{
+    if (a.getRawBits() > b.getRawBits())
+        return a;
+    return b;
+}
+
+std::ostream& operator<<(std::ostream& os, Fixed const& f)
+{
+    os << f.toFloat();
+    return os;
+}
---
+++ Module02/ex02/Fixed.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 15:08:06 by sechlahb          #+#    #+#             */
+/*   Updated: 2025/11/07 18:42:18 by sechlahb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class  Fixed
+{
+    private:
+        int                 _fixedPointValue;
+        static const int    _fractionalBits;
+    public:
+        Fixed();
+        Fixed(const int n);
+        Fixed(const float f);
+        Fixed(const Fixed& original);
+        Fixed& operator=(const Fixed& original);
+        ~Fixed();
+
+        /* comparison operators*/
+        bool operator>(const Fixed& original);
+        bool operator<(const Fixed& original);
+        bool operator>=(const Fixed& original);
+        bool operator<=(const Fixed& original);
+        bool operator==(const Fixed& original);
+        bool operator!=(const Fixed& original);
+
+        /*arithmetic operators*/
+        Fixed& operator+(const Fixed& original);
+        Fixed& operator-(const Fixed& original);
+        Fixed& operator*(const Fixed& original);
+        Fixed& operator/(const Fixed& original);
+    
+        /*increment and decrement*/
+        Fixed& operator++(int); // i++
+        Fixed& operator--(int); // i--
+        Fixed& operator++(); // ++i
+        Fixed& operator--(); // --i
+
+        /*min*/
+        static Fixed& min(Fixed& a, Fixed& b);
+        static Fixed& min(const Fixed& a, const Fixed& b);
+
+        /*max*/
+        static Fixed& max(Fixed& a, Fixed& b);
+        static Fixed& max(const Fixed& a, const Fixed& b);
+        
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+    
+        float   toFloat() const;
+        int     toInt() const;
+        
+};
+
+std::ostream& operator<<(std::ostream& os, Fixed const& f);
+
+#endif /*FIXED_HPP*/
