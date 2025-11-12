0be9e968ad09a23568d65f780038b3d7abc2c4d2
said echlahbi <echlahbisaid@gmail.com>
2025-11-12 22:36:20 +0100

    update

---
+++ Module03/ex01/ScavTrap.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 21:28:43 by sechlahb          #+#    #+#             */
+/*   Updated: 2025/11/12 21:42:42 by sechlahb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP.HPP
+#define SCAVTRAP.HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap  
+
+
+#endif /*SCAVTRAP.HPP*/
