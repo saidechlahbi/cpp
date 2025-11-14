3131a84fe1c87ad9193ce540f90037889ecf862d
said echlahbi <echlahbisaid@gmail.com>
2025-11-14 16:44:06 +0100

    gg

--- Module01/ex04/sed.cpp
+++ Module01/ex04/sed.cpp
@@ -3,10 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   sed.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sechlahb <sechlahb@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 23:41:08 by sechlahb          #+#    #+#             */
-/*   Updated: 2025/10/24 23:41:09 by sechlahb         ###   ########.fr       */
+/*   Updated: 2025/11/14 16:32:57 by sechlahb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "sed.hpp"
+
+std::string content_rep(std::string line , std::string s1, std::string s2)
+{
+    std::string result;
+    size_t last_pos = 0;
+    std::size_t pos = line.find(s1);
+
+    while(pos > 0)
+    {
+        result.append(line, last_pos, pos - last_pos);
+        result.append(s2);
+        last_pos = pos + s1.length();
+        pos = line.find(s1, last_pos);
+    }
+    result.append(line, last_pos, line.length() - last_pos);
+    return result;
+}
--- Module01/ex04/sed.hpp
+++ Module01/ex04/sed.hpp
@@ -3,10 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   sed.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sechlahb <sechlahb@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 23:41:21 by sechlahb          #+#    #+#             */
-/*   Updated: 2025/10/24 23:41:22 by sechlahb         ###   ########.fr       */
+/*   Updated: 2025/11/14 16:21:43 by sechlahb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SED_HPP
+#define SED_HPP
+
+#include <iostream>
+#include <fstream>
+
+std::string content_rep(std::string line , std::string s1, std::string s2);
+
+#endif /*SED_HPP*/
