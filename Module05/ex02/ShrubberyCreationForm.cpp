8f88fe59041791deb52d05e9e7dbc9da88631c26
Said Echlahbi <sechlahb@c2r9p2.1337.ma>
2026-02-11 15:27:29 +0100

    GG

--- Module05/ex02/ShrubberyCreationForm.cpp
+++ Module05/ex02/ShrubberyCreationForm.cpp
@@ -1,16 +1,23 @@
 #include "ShrubberyCreationForm.hpp"
 
 ShrubberyCreationForm::ShrubberyCreationForm():AForm(),_target("default"){}
-ShrubberyCreationForm::ShrubberyCreationForm(std::string target):AForm(),_target(target)
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target):AForm(),_target(target){}
 ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &other): AForm(other), _target(other._target){}
 ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
 {
     if (this != &other)
     {
-        
+        AForm::operator=(other);
         _target = other._target;
     }
     return *this;
 }
-ShrubberyCreationForm::~ShrubberyCreationForm();
-void ShrubberyCreationForm::beSigned(Bureaucrat& boss);
+ShrubberyCreationForm::~ShrubberyCreationForm(){}
+
+void ShrubberyCreationForm::beSigned(Bureaucrat& boss)
+{
+    if (boss.getGrade() >= _requireGrade)
+        _signature = true;
+    else
+        throw AForm::GradeTooLowException();
+}
