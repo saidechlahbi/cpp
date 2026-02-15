bd9c73645892e659905bdbdc2feaf68dc91d8ad1
Said Echlahbi <sechlahb@c3r8p2.1337.ma>
2026-02-15 19:48:47 +0000

    update

---
+++ Module05/ex03/Intern.cpp
@@ -0,0 +1,14 @@
+#include "Intern.hpp"
+
+
+Intern::Intern(){}
+Intern::Intern(const Intern* other){}
+
+Intern* Intern::operator=(const Intern *other){}
+
+Intern::~Intern(){}
+
+AForm* Intern::makeForm(std::string form_name, std::string form_target)
+{
+
+}
