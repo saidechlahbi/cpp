9e95377744525940d15d7edba435e3be0c7442a1
Said Echlahbi <sechlahb@c2r9p14.1337.ma>
2026-02-21 15:27:37 +0000

    exercice 1 Module6

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -1,5 +1,5 @@
 #include "ScalarConverter.hpp"
-
+#include <cmath>
 
 ScalarConverter::ScalarConverter(){}
 ScalarConverter::ScalarConverter(const ScalarConverter& other)
@@ -14,14 +14,31 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 ScalarConverter::~ScalarConverter(){}
 
 
-
-
-
 void ScalarConverter::convert(std::string input)
 {
-    if (input == "-inff")
-    if (input == "+inff")
-    if (input == "-inf")
-    if (input == "+inf")
-    if (input == "nan")
+    char *iterater;
+    iterater = NULL;
+    double value = strtod(input, &iterater);
+    if (iterater[0] == '\0' || (iterater[0] == 'f' && iterater[1] == '\0'))
+    {
+        if ( value <= 31  ||  value >= 127)
+            std::cout << "char: Non displayable" << std::endl;
+        else if (std::isinf(value) || std::isnan(value))
+            std::cout << "char: impossible" << std::endl;
+        else
+            std::cout << "char: " << static_cast<char>(value) << std::endl;
+
+        if (std::isinf(value) || std::isnan(value))
+            std::cout << "int: impossible" << std::endl;
+        else
+            std::cout << "int: " << static_cast<int>(value) << std::endl;
+        
+        std::cout << "float: " << static_cast<float>(value) << 'f' << std::endl;
+        std::cout << "double: " << value << std::endl;
+    }
+    else
+    {
+        std::cout << "invalid input!" << std::endl;
+    }
+
 }
