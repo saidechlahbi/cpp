c783df4613fe2a0d62b79058eef650784107c891
saidechlahbi <echlahbisaid@gmail.com>
2026-03-09 14:23:00 +0000

    update

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -1,10 +1,11 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
 #include <stack>
 class MutantStack: public stack
 {
     
-}
+};
 
 #endif /*MUTANTSTACK_HPP*/
--- test.cpp
+++ test.cpp
@@ -1,61 +1,64 @@
-// #include <iostream>
-// #include <string>
-// #include <vector>
 
-// #include <list>
-// using namespace std;
+ #include <cstddef>
+#include <list>
+ #include <iostream>
+#include <system_error>
 
-// typedef list<int> ListNode;
-
-// ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//     list<int>::iterator it = l1->begin();
-//     int total_l1 = *it;
-//     it++;
-//     while(it != l1->end())
-//     {
-//         total_l1 *= 10;
-//         total_l1 += *it;
-//         it++;
-//     }
-
-//     list<int>::reverse_iterator rit = l2->rbegin();
-//     int total_l2 = *rit;
-//     rit++;
-//     while(rit != l2->rend())
-//     {
-//         total_l2 *= 10;
-//         total_l2 += *rit;
-//         rit++;
-//     }
-//     int total = total_l1 + total_l2;
-//     ListNode *mylist = new ListNode();
-//     while (total)
-//     {
-//         mylist->push_front(total%10);
-//         total /= 10;
-//     }
-//     return mylist;
-// }
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 
-// int main ()
-// {
-//     list<int> l1 = {2,4,3};
-//     list<int>  l2 = {5,6,4};
-//     list<int> *list = addTwoNumbers(&l1, &l2);
-//     for(ListNode::iterator it = list->begin(); it != list->end(); it++)
-//         cout << *it << "\t";
-//     cout << endl;
-// }
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+{
+  int total_l1 = 0;
+  total_l1 = l1->val;
+  l1 = l1->next;
+  while (l1->next != NULL)
+  {
+    total_l1 = total_l1 * 10;
+    total_l1 += l1->val;
+  }
 
-// using standard exceptions
-#include <iostream>
-#include <exception>
-#include <algorithm>
-using namespace std;
 
-#include <vector>
-int main () {
-  std::vector<int> vec = {2,4,6,7};
-  std::vector<int>::iterator it = find(vec.begin(), vec.end(), 5);
+    int total_l2 = 0;
+  total_l2 = l2->val;
+  l2 = l2->next;
+  while (l2->next != NULL)
+  {
+    total_l2 = total_l2 * 10;
+    total_l2 += l2->val;
+  }
+  
+  int reverse_l2 = total_l2 % 10;
+  total_l2 /= 10;
+
+  while (total_l2)
+  {
+    reverse_l2 = reverse_l2 * 10;
+    reverse_l2 += total_l2 % 10;
+  }
+  int result = total_l1 + reverse_l2;
+  ListNode *head = new ListNode();
+  ListNode *tmp;
+  ListNode *it;
+  head->val = result % 10;
+  head->next = NULL;
+  result /= 10;
+  while (result)
+  {
+    tmp = new ListNode();
+    tmp->val = result % 10;
+    tmp->next = NULL;
+    result /= 10;
+    it = head;
+    while (it->next != NULL)
+      it = it->next;
+    it->next = tmp;
+  }
+  return head;
 }
